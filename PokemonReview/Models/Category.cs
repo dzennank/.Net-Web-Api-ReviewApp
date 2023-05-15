74df9f9d5960281e2a457b43118eff3d7462fbdc
dzennank <dzenankrlic@gmail.com>
2023-05-15 16:53:04 +0200

    set up EF, data context, repository pattern

--- PokemonReview/Models/Category.cs
+++ PokemonReview/Models/Category.cs
@@ -4,6 +4,6 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        ICollection<PokemonCategory> PokemonCategories { get; set;}
+        public ICollection<PokemonCategory> PokemonCategories { get; set;}
     }
 }
---
+++ PokemonReview/Repository/PokemonRepository.cs
@@ -0,0 +1,20 @@
+﻿using PokemonReview.Data;
+using PokemonReview.Models;
+
+namespace PokemonReview.Repository
+{
+    public class PokemonRepository
+    {
+        private readonly DataContext _context;
+
+        public PokemonRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public ICollection<Pokemon> GetPokemons()
+        {
+            return _context.Pokemon.OrderBy(p => p.Id).ToList();
+        }
+    }
+}
