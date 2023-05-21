2fda6f91394231135b7bed1db1415549e2d87ecb
dzennank <dzenankrlic@gmail.com>
2023-05-21 15:38:49 +0200

    Reviewer Interface

---
+++ PokemonReview/Interfaces/IReviwerRepository.cs
@@ -0,0 +1,13 @@
+﻿using PokemonReview.Models;
+
+namespace PokemonReview.Interfaces
+{
+    public interface IReviwerRepository
+    {
+        List<Reviewer> GetReviewers();
+        Reviewer GetReviewer(int id);
+        List<Review> GetReviewByReviewer(int reviewerId);
+
+        bool ReviewerExists(int id);
+    }
+}
