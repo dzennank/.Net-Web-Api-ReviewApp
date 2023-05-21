28da7f4f4cd2f4c93ab2d2c1df79104a0f3c4423
dzennank <dzenankrlic@gmail.com>
2023-05-21 15:39:54 +0200

    Reviewer repo and controller

---
+++ PokemonReview/Controllers/ReviewerController.cs
@@ -0,0 +1,62 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PokemonReview.Dto;
+using PokemonReview.Interfaces;
+using PokemonReview.Models;
+
+namespace PokemonReview.Controllers
+{
+    [Route("api/reviewer")]
+    [ApiController]
+    public class ReviewerController : Controller
+    {
+        private readonly IReviwerRepository _reviwerRepository;
+        private readonly IMapper _mapper;
+
+        public ReviewerController(IReviwerRepository reviwerRepository, IMapper mapper)
+        {
+            _reviwerRepository = reviwerRepository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(200, Type = typeof(List<Reviewer>))]
+        public IActionResult GetReviewers() 
+        { 
+            if(!ModelState.IsValid)
+                return BadRequest(ModelState);
+            var getReviewers = _reviwerRepository.GetReviewers();
+            var reviewers = _mapper.Map<List<ReviewerDto>>(getReviewers).ToList();
+            return Ok(reviewers);
+
+        }
+
+        [HttpGet("{reviewerId}")]
+        [ProducesResponseType(200, Type = typeof(Reviewer))]
+        public IActionResult GetReviewer(int reviewerId)
+        {
+            if (!_reviwerRepository.ReviewerExists(reviewerId))
+                return NotFound();
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            var getReviewer = _reviwerRepository.GetReviewer(reviewerId);
+            var reviewer = _mapper.Map<ReviewerDto>(getReviewer);
+            return Ok(reviewer);
+
+        }
+        [HttpGet("reviewByReviewer/{reviewerId}")]
+        [ProducesResponseType(200, Type = typeof(List<Reviewer>))]
+
+        public IActionResult GetReviewByReviewer(int reviewerId)
+        {
+            if(!_reviwerRepository.ReviewerExists(reviewerId))
+                return NotFound();
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+             var getReview = _reviwerRepository.GetReviewByReviewer(reviewerId);
+            var review = _mapper.Map<List<ReviewDto>>(getReview);
+            return Ok(review);
+
+        }
+    }
+}
---
+++ PokemonReview/Repository/ReviewerRepository.cs
@@ -0,0 +1,35 @@
+﻿using PokemonReview.Data;
+using PokemonReview.Interfaces;
+using PokemonReview.Models;
+
+namespace PokemonReview.Repository
+{
+    public class ReviewerRepository : IReviwerRepository
+    {
+        private readonly DataContext _context;
+
+        public ReviewerRepository(DataContext context)
+        {
+            _context = context;
+        }
+        public List<Review> GetReviewByReviewer(int reviewerId)
+        {
+            return _context.Reviews.Where(r => r.Reviewer.Id == reviewerId).ToList();
+        }
+
+        public Reviewer GetReviewer(int id)
+        {
+            return _context.Reviewers.Where(r => r.Id == id).FirstOrDefault();
+        }
+
+        public List<Reviewer> GetReviewers()
+        {
+            return _context.Reviewers.OrderBy(r => r.Id).ToList();
+        }
+
+        public bool ReviewerExists(int id)
+        {
+            return _context.Reviewers.Any(r => r.Id == id);
+        }
+    }
+}
